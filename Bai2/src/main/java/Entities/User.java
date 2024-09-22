a883f108cd3715c952e58320f56ea229e6950575
unknown <vunguyenminhduc1410@gmail.com>
2024-09-22 22:58:38 +0700

    hoan thanh bai tap tuan 3

---
+++ Bai2/src/main/java/Entities/User.java
@@ -0,0 +1,92 @@
+package Entities;
+
+import java.util.Date;
+
+public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private Date birthday;
+	private Gender gender;
+
+	public User(int id, String firstName, String lastName, String email, String password, Date birthday,
+			Gender gender) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.birthday = birthday;
+		this.gender = gender;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", birthday=" + birthday + ", gender=" + gender + "]";
+	}
+
+}
---
+++ Bai2/src/main/java/daoImpl/UserDAOImpl.java
@@ -0,0 +1,93 @@
+package daoImpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import Entities.User;
+import dao.UserDAO;
+
+public class UserDAOImpl implements UserDAO {
+	private DataSource datasource;
+
+	public UserDAOImpl(DataSource datasource) {
+		super();
+		this.datasource = datasource;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		String sql = "SELECT * FROM users";
+		List<User> list = new ArrayList<User>();
+		try (Connection con = this.datasource.getConnection();
+				PreparedStatement ps = (PreparedStatement) con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();) {
+			while (rs.next()) {
+				User user = new User();
+				user.setId(rs.getInt("id"));
+				user.setFirstName(rs.getString("first_name"));
+				user.setLastName(rs.getString("last_name"));
+				user.setEmail(rs.getString("email"));
+				user.setPassword(rs.getString("password"));
+				user.setBirthday(rs.getDate("birthday"));
+				list.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public User getById(int id) {
+		String sql = "SELECT * FROM users WHERE id=?";
+		User user = null;
+		try (Connection con = this.datasource.getConnection();
+				PreparedStatement ps = (PreparedStatement) con.prepareStatement(sql);) {
+			ps.setInt(1, id);
+			try (ResultSet rs = ps.executeQuery();) {
+				if (rs.next()) {
+					user = new User();
+					user.setId(rs.getInt("id"));
+					user.setFirstName(rs.getString("first_name"));
+					user.setLastName(rs.getString("last_name"));
+					user.setEmail(rs.getString("email"));
+					user.setPassword(rs.getString("password"));
+					user.setBirthday(rs.getDate("birthday"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	@Override
+	public void addUser(User p) {
+		String sql = "INSERT INTO users(first_name, last_name, email, password, birthday) VALUES(?,?,?,?,?)";
+		try (Connection con = this.datasource.getConnection();
+				PreparedStatement ps = (PreparedStatement) con.prepareStatement(sql);) {
+			ps.setString(1, p.getFirstName());
+			ps.setString(2, p.getLastName());
+			ps.setString(3, p.getEmail());
+			ps.setString(4, p.getPassword());
+			if (p.getBirthday() != null) {
+				java.util.Date utilDate = p.getBirthday();
+				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+				ps.setDate(5, sqlDate);
+			} else {
+				ps.setDate(5, null);
+			}
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
